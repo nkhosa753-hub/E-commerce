1b264547601f8973205d0cec56c4ec544221fb9f
Naveed Khosa <naveedkhosa2006@gmail.com>
2025-11-22 13:57:59 +0500

    d

--- server/vite.ts
+++ server/vite.ts
@@ -68,7 +68,7 @@ export async function setupVite(app: Express, server: Server) {
 }
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(process.cwd(), "dist");
+  const distPath = path.resolve(process.cwd(), "dist-server", "dist");
   const distServerPath = path.resolve(process.cwd(), "dist-server");
   const rootPath = process.cwd();
 
